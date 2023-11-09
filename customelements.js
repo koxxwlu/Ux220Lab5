499cfbfaac7773e0cfde636925be0ed9b9a041e1
koxxwlu <koxx0579@mylaurier.ca>
2023-11-08 20:45:19 -0500

    udpate customelements.js

--- customelements.js
+++ customelements.js
@@ -12,3 +12,10 @@ class CentreElement extends HTMLElement {
     }
 }
 customElements.define("x-centre", CentreElement);
+
+class DateElement extends HTMLElement {
+    connectedCallback(){
+        this.innerHTML = new Date().getFullYear();
+    }
+}
+customElements.define("x-fullyear", DateElement);
